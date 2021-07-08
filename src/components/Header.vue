8b98258f2730fdbaa7423d549069e7f2e0f4f88e
leonardo <sponzal4@gmail.com>
2021-07-08 19:12:20 +0200

    axios

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  
+  <header class=".bg-dark.bg-gradient">
+      <img class="w-25 p-3  h-100 d-inline-block " src="@/assets/logo.png" alt="">
+  </header>
 </template>
 
 <script>
 export default {
-
+    
 }
 </script>
 
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+      <div class="row .bg-dark.bg-gradient">
+          <div class="col-5">
+
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data(){
+        return{
+            apiURL:'https://flynn.boolean.careers/exercises/api/array/music',
+            List : []
+        }
+    },
+    created(){
+        this.getList();
+    },
+    methods:{
+        getList(){
+            axios
+            .get(this.apiURL)
+            .then(res => {
+                console.log(res.data);
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
